6227fe5ae4cef936c1ebaab5b4d86e43d05114ee
palaciosalex <palaciosalex07@gmail.com>
2021-11-14 23:26:53 -0500

    configuracion

--- MuebleriaDjango/urls.py
+++ MuebleriaDjango/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import settings
-import static
+from django.conf import settings
+from django.conf.urls.static import static
 from Ecommercee.views import login,home,productos, validar
 
 urlpatterns = [
